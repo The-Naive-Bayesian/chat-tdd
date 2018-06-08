d60bfa4991ac6a01b4a90a9a73d7880e924110c7
Chris Emerson <cemerson@den-lt-cemerson.domain>
2018-06-08 12:37:20 -0600

    Write tests and code for chat-session

--- chat-session.spec.ts
+++ chat-session.spec.ts
@@ -1,8 +1,9 @@
 import {ChatSession} from "./chat-session";
+import {expect} from 'chai';
 
 describe('ChatSession', function() {
     it('should build', function() {
-        const io = {};
-        new ChatSession(io);
+        const socket = {};
+        new ChatSession(socket);
     });
 });
