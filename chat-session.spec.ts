d20679012b5c72d4ae627899a454026d64dd85fc
Chris Emerson <cemerson@den-lt-cemerson.domain>
2018-06-08 16:51:53 -0600

    WIP: first test and mods for making socket events work

--- chat-session.spec.ts
+++ chat-session.spec.ts
@@ -1,9 +1,27 @@
 import {ChatSession} from "./chat-session";
 import {expect} from 'chai';
+import * as EventEmitter from 'events';
+import {fake} from 'sinon';
 
 describe('ChatSession', function() {
     it('should build', function() {
-        const socket = {};
-        new ChatSession(socket);
+        const socket = {on: (event, callback)=>{}};
+        new ChatSession(socket, ()=>{});
+    });
+    it('should invoke callback on "message" event', function(done) {
+        const socket = new EventEmitter;
+        const _fake = fake();
+        const callback = () => {
+            _fake();
+            expect(_fake.calledOnce).to.be.true;
+            done();
+        };
+        new ChatSession(socket, callback);
+        socket.emit('message');
+    });
+    it('should be true', function() {
+        const _fake = fake();
+        _fake();
+        expect(_fake.calledOnce).to.be.true;
     });
 });
--- chat-session.ts
+++ chat-session.ts
@@ -4,6 +4,10 @@
     We create a class here that is instantiated on connection and contains event handling logic.
  */
 export class ChatSession {
-    constructor(private socket: any) {
+    constructor(
+        private socket: {on: (event: string, callback: any) => void},
+        callback: () => void
+    ) {
+        socket.on('message', callback);
     }
 }
