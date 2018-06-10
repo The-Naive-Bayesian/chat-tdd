65a8787e53865d84fb64e3a26c941b3feb03fdb2
Christopher Emerson <chris@ChristohersMBP2.domain>
2018-06-10 14:06:52 -0600

    Built event-handler.ts and test for eventual implementation

---
+++ event-handlers.spec.ts
@@ -0,0 +1,13 @@
+import {expect} from 'chai';
+import {MockSocket} from "./mock-classes/MockSocket";
+import {fake} from 'sinon';
+import {messageHandler} from "./event-handlers";
+
+describe('messageHandler', function() {
+    it('calls broadcast on the socket', function() {
+        const _fake = fake();
+        const mockSock = new MockSocket;
+        messageHandler({message: ''}, mockSock);
+        expect(_fake.calledOnce).to.be.true;
+    });
+});
