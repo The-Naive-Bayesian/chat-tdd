d991f897e1ff226a266eb6681489ac8b0e815fd9
Christopher Emerson <chris@ChristohersMBP2.domain>
2018-06-10 14:00:27 -0600

    Updated MockSocket to allow passing in fake for broadcast.emit

--- mock-classes/MockSocket.ts
+++ mock-classes/MockSocket.ts
@@ -1,5 +1,14 @@
 import {EventEmitter} from 'events';
 
 export class MockSocket extends EventEmitter {
-    broadcast = {emit: this.emit}
+    broadcast: {emit: (event: string | symbol, ...args: any[]) => boolean};
+
+    constructor(broadcastEmit?: (event: string | symbol, ...args: any[]) => boolean) {
+        super();
+        if (broadcastEmit) {
+            this.broadcast.emit = broadcastEmit;
+        } else {
+            this.broadcast.emit = this.emit;
+        }
+    }
 }
