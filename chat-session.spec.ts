9a067dc97b4db28ba90751274eedc323f07712f4
Christopher Emerson <chris@ChristohersMBP2.domain>
2018-06-09 22:57:44 -0600

    Created model for socket to simplify typing

--- chat-session.spec.ts
+++ chat-session.spec.ts
@@ -3,13 +3,17 @@ import {expect} from 'chai';
 import * as EventEmitter from 'events';
 import {fake} from 'sinon';
 
+class MockSocket extends EventEmitter {
+    broadcast = {emit: this.emit}
+}
+
 describe('ChatSession', function() {
     it('should build', function() {
-        const socket = {on: (event, callback)=>{}};
+        const socket = new MockSocket;
         new ChatSession(socket, ()=>{});
     });
     it('should invoke callback on "message" event', function(done) {
-        const socket = new EventEmitter;
+        const socket = new MockSocket;
         const _fake = fake();
         const callback: (data) => void = (data) => {
             _fake();
@@ -20,7 +24,7 @@ describe('ChatSession', function() {
         socket.emit('message');
     });
     it('should invoke callback with "data" object argument on "message" event', function(done) {
-        const socket = new EventEmitter;
+        const socket = new MockSocket;
         const callback: (data: any) => void = (data) => {
             expect(typeof data).to.equal('object');
             done();
@@ -29,7 +33,7 @@ describe('ChatSession', function() {
         socket.emit('message', {});
     });
     it('"message" event "data" argument should contain string property "message"', function(done) {
-        const socket = new EventEmitter;
+        const socket = new MockSocket;
         const callback: (data: {message: string}) => void = (data) => {
             expect(typeof data.message).to.equal('string');
             done();
@@ -38,7 +42,7 @@ describe('ChatSession', function() {
         socket.emit('message', {message: 'hello, world'});
     });
     it('should pass the socket as the second argument to the "message" event', function(done) {
-        const _socket = new EventEmitter;
+        const _socket = new MockSocket;
         const callback: (data: {message: string}, socket: any) => void = (data, socket) => {
             expect(socket).to.equal(_socket);
             done();
--- chat-session.ts
+++ chat-session.ts
@@ -1,13 +1,13 @@
+import {SocketModel} from "./models/socket.model";
 /*
     To keep things testable, we want to use DI with our socket.
     This allows us to mock the socket.io objects and test our methods.
     We create a class here that is instantiated on connection and contains event handling logic.
  */
+
 export class ChatSession {
     constructor(
-        private socket: {
-            on: (event: string, callback: (data: any) => void) => void
-        },
+        private socket: SocketModel,
         callback: (data: any, socket: any) => void
     ) {
         socket.on('message', (data) => {
